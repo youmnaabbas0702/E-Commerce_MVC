3c81e0c2df5f73cca5ded61555f55fa3672fb81c
youmna <yomnaa.2002.ya@gmail.com>
2024-10-31 22:06:24 +0300

    System Architecture

---
+++ Application/eCommerce.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using eCommerce.DAL.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.DAL.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ Application/eCommerce.DAL/Models/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace eCommerce.DAL.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; }
+    }
+}
